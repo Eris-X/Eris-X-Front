bd3c0277a1246a9af187320f38793782c54dc34c
zhangwei <zhangweib@wxblockchain.com>
2023-03-28 11:59:57 +0800

    optimize realtime package

--- crowd-sourcing-project/src/main.js
+++ crowd-sourcing-project/src/main.js
@@ -9,12 +9,15 @@ import App from './App.vue'
 import router from './router'
 
 import Cookies from 'js-cookie'
+import Web3 from 'web3'
+
 
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium' // set element-ui default size
 })
 
 Vue.config.productionTip = false
+Vue.prototype.Web3 = Web3
 
 new Vue({
   router,
--- crowd-sourcing-project/src/views/wallet/index.vue
+++ crowd-sourcing-project/src/views/wallet/index.vue
@@ -2,10 +2,19 @@
   <div class="wallet">
     <div class="container">
       <div class="my">
-        <h4 style="margin: 0 0 20px;font-size: 20px;">我的钱包地址: 0x4395****745</h4>
+        <h4 style="margin: 0 0 20px;font-size: 20px;">我的钱包地址: {{ walletAddress }}</h4>
+        <button @click="get">连接钱包</button>
+        <button @click="getTokenBalance()">查余额</button>
+        <button @click="tokenTransfer()">转账代币</button>
+        <button @click="signMessage()">签名</button>
+        <button @click="getAllowance">查询授权金额</button>
+        <button @click="getApprove">授权</button>
+
+
         <div class="card-container">
           <div class="card">
-            <div>余额：100usdt</div>
+            <div>余额：{{tokenBalance}}</div>
+            <div>授权额度：{{allowance}}</div>
             <div>已质押：100usdt</div>
           </div>
           <div class="card">
@@ -38,8 +47,14 @@
   </div>
 </template>
 <script>
+import Web3 from 'web3'
+import abi from '../../abi/ERC20.json'
+import BigNumber from 'bignumber.js';
 export default {
   name: 'Wallet',
+  mounted() {
+    this.initWeb3();
+  },
   data() {
     return {
       list: [
@@ -59,7 +74,121 @@ export default {
           money: '90,000',
           time: '2023/01/1-2022/02/12'
         },
-      ]
+      ],
+      walletAddress: "",
+      tokenBalance:0,
+      tokenERCAddr:"0xaE2ABEFFccC28078EbA6EACE997eC52D40135450",
+      web3: null,
+      ethContract:null,
+      allowance:0
+
+    }
+  },
+  methods: {
+    async initWeb3() {
+      if (typeof window.ethereum !== 'undefined') {
+        // 使用 MetaMask
+        this.web3 = new Web3(window.ethereum);
+        try {
+          // 请求用户授权
+          await window.ethereum.enable();
+        } catch (error) {
+          console.error('User denied account access');
+        }
+      } else if (typeof window.web3 !== 'undefined') {
+        // 使用旧版 MetaMask
+        this.web3 = new Web3(window.web3.currentProvider);
+      } else {
+        // 如果用户没有安装 MetaMask，则使用 Infura 作为 Provider
+        console.warn('No Web3 detected');
+        const provider = new Web3.providers.HttpProvider('https://devnet2openapi.platon.network/rpc', null, {chainId: 2206132});
+        this.web3 = new Web3(provider);
+      }
+      this.ethContract = new this.web3.eth.Contract(abi, this.tokenERCAddr) //所有代币的abi可以通用（abi,合约地址）
+
+    },
+    getData() {
+      console.log('first')
+    },
+    get() {  // 唤起钱包
+      if (window.ethereum) {
+        window.ethereum.enable().then((res) => {
+          this.walletAddress = res[0];
+          // alert("当前钱包地址:" + res[0]);
+        });
+      } else {
+        alert("请安装MetaMask钱包");
+      }
+    },
+    //查询代币余额
+    async getTokenBalance() {
+      if (this.ethContract) {
+        let accounts = await this.web3.eth.getAccounts()
+        let decimals = await this.ethContract.methods.decimals().call()
+        this.tokenBalance = await this.ethContract.methods.balanceOf(accounts[0]).call() / Math.pow(10, decimals)
+      }
+    },
+    //token转账
+    async tokenTransfer() {
+      let accounts = await this.web3.eth.getAccounts()
+      let decimals = await this.ethContract.methods.decimals().call()
+      let amount = new BigNumber(1 * Math.pow(10, decimals));
+      const maxUint256 = new BigNumber('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff');
+      if (amount.gt(maxUint256)) {
+        console.error('Amount exceeds maximum uint256 value.');
+        return;
+      }
+
+      let toAddress = "0x9eBd6e81941FB5df5f22A4dBB3735f4e53280E68";
+      console.log(accounts[0])
+      let options = {
+        from: accounts[0], // 发送者的地址
+      };
+      await this.ethContract.methods.transfer(toAddress, amount).send(options)
+    },
+    async signMessage() {
+      if (typeof window.ethereum !== 'undefined') {
+        try {
+          // 请求用户授权
+          await window.ethereum.enable();
+          // 发送签名请求
+          const accounts = await window.ethereum.request({
+            method: 'eth_requestAccounts',
+          });
+          const account = accounts[0];
+          const signature = await window.ethereum.request({
+            method: 'personal_sign',
+            params: [
+              'Hello, world!',
+              account
+            ]
+          });
+          console.log('Signature: ', signature);
+        } catch (error) {
+          console.error('User denied account access');
+        }
+      } else {
+        console.error('Web3 not found');
+      }
+    },
+    //查询授权金额
+    async getAllowance(){
+      if(window.web3) {
+        let accounts = await this.web3.eth.getAccounts()
+        let decimals = await this.ethContract.methods.decimals().call()
+        this.allowance = await this.ethContract.methods.allowance(accounts[0],this.tokenERCAddr).call()/Math.pow(10,decimals)
+      }
+    },
+
+    //授权
+    async getApprove() {
+      if (window.web3) {
+        let decimals = await this.ethContract.methods.decimals().call()
+        let amount =new BigNumber(100 * Math.pow(10, decimals));//转账100个
+        //小狐狸账户
+        let accounts = await this.web3.eth.getAccounts()
+        this.ethContract.methods.approve(this.tokenERCAddr, amount + '').send({from: accounts[0]})
+      }
     }
   }
 }
